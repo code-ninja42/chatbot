b280487c80df0cdda1b3b59eab291a5cd756436f
code-ninja42 <143476719+code-ninja42@users.noreply.github.com>
2026-09-08 11:36:20 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -6,11 +6,14 @@ from products import router as product_router
 from order import router as order_router
 from chatbot import ask_chatbot
 
+
 app = FastAPI(
     title="Store Chatbot API",
     version="1.0"
 )
 
+
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -19,20 +22,32 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+# Product APIs
 app.include_router(product_router)
+
+# Order APIs
 app.include_router(order_router)
 
 
+# Chat request
 class ChatRequest(BaseModel):
     question: str
 
 
+# Chatbot API
 @app.post("/chat")
 def chat(request: ChatRequest):
     answer = ask_chatbot(request.question)
-    return {"answer": answer}
+
+    return {
+        "answer": answer
+    }
 
 
+# Home API
 @app.get("/")
 def home():
-    return {"message": "Store Chatbot API is running"}
+    return {
+        "message": "Store Chatbot API is running"
+    }
